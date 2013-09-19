703e3c64ac9b133ebd7b6da677f3cbcf85ad67b8
Tina Holly (Tinacious Design) <tinacious.design@gmail.com>
2013-09-18 22:44:04 -0400

    repsonsive tables for expensr

--- js/expensr.js
+++ js/expensr.js
@@ -1,4 +1,12 @@
 ﻿$(document).ready(function () {
+    // Responsive Tables
+    var mobile = 'only all and (max-width: 500px)';
+
+    // if(Modernizr.mq(mobile)) {
+    //     $('.responsive-table').stacktable();
+    // }
+    
+    // Math
     MyApp = {};
     MyApp.expenses = [];
     MyApp.Expensr = function Expensr(a, b, c, d, e) {
@@ -32,6 +40,14 @@
         MyApp.calculateDebts();
         MyApp.displayExpenses();
         $('input[type=text]').val('');
+
+        // Execute responsive tables here?
+        if(Modernizr.mq(mobile)) {
+            $('.stacktable').remove();
+            $('.responsive-table').stacktable();
+            $('.responsive-table .st-key .lsf-icon').remove();
+        }
+        
     }
     MyApp.removeExpense = function (index) {
         MyApp.expenses.splice(index, 1);
@@ -156,3 +172,64 @@
         }
     }
 });
+
+// Plugins
+/**
+ * stacktable.js
+ * Author & copyright (c) 2012: John Polacek
+ * Dual MIT & GPL license
+ *
+ * Page: http://johnpolacek.github.com/stacktable.js
+ * Repo: https://github.com/johnpolacek/stacktable.js/
+ *
+ * jQuery plugin for stacking tables on small screens
+ *
+ */
+;(function($) {
+
+  $.fn.stacktable = function(options) {
+    var $tables = this,
+        defaults = {id:'stacktable',hideOriginal:false},
+        settings = $.extend({}, defaults, options),
+        stacktable;
+
+    return $tables.each(function() {
+      var $stacktable = $('<table class="'+settings.id+'"><tbody></tbody></table>');
+      if (typeof settings.myClass !== undefined) $stacktable.addClass(settings.myClass);
+      var markup = '';
+      $table = $(this);
+      $topRow = $table.find('tr').eq(0);
+      $table.find('tr').each(function(index,value) {
+        markup += '<tr>';
+        // for the first row, top left table cell is the head of the table
+        if (index===0) {
+          markup += '<tr><th class="st-head-row st-head-row-main" colspan="2">'+$(this).find('th,td').eq(0).html()+'</th></tr>';
+        }
+        // for the other rows, put the left table cell as the head for that row
+        // then iterate through the key/values
+        else {
+          $(this).find('td').each(function(index,value) {
+            if (index===0) {
+              markup += '<tr><th class="st-head-row" colspan="2">'+$(this).html()+'</th></tr>';
+            } else {
+              if ($(this).html() !== ''){
+                markup += '<tr>';
+                if ($topRow.find('td,th').eq(index).html()){
+                  markup += '<td class="st-key">'+$topRow.find('td,th').eq(index).html()+'</td>';
+                } else {
+                  markup += '<td class="st-key"></td>';
+                }
+                markup += '<td class="st-val">'+$(this).html()+'</td>';
+                markup += '</tr>';
+              }
+            }
+          });
+        }
+      });
+      $stacktable.append($(markup));
+      $table.before($stacktable);
+      if (settings.hideOriginal) $table.hide();
+    });
+  };
+
+}(jQuery));
